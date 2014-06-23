caf2e7e8c1d652e8eab4b173b3367f5263b1b235
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-23 11:10:43 +1000

    Basic working version

--- index.js
+++ index.js
@@ -12,10 +12,50 @@
 
   The following test steps are executed as part of the testing cycle:
 
-  -
+  - Test audio output (using the Audio API)
+  - Test capture using local media capture
 
   ## Icon Licenses
 
   <<< docs/icons.md
 
 **/
+
+var crel = require('crel');
+var svg = require('svg');
+var fs = require('fs');
+var DEFAULT_STEPS = [
+  'welcome',
+  'audio',
+  'capture',
+  'connect'
+];
+
+var icons = {
+  audio: fs.readFileSync(__dirname + '/icons/icon_21365.svg', 'utf8'),
+  capture: fs.readFileSync(__dirname + '/icons/icon_28712.svg', 'utf8')
+};
+
+function createStep(name) {
+  return crel('section', {
+    class: 'rtc-wizard-step',
+    'data-step': name
+  }, svg(icons[name]));
+}
+
+module.exports = function(opts) {
+  var steps = (opts || {}).steps || DEFAULT_STEPS;
+  var container = crel('div', {
+    class: 'rtc-wizard rtc-setup'
+  });
+
+  // create the child elements
+  var wizardSteps = steps.map(createStep);
+
+  // add the steps to the container
+  wizardSteps.forEach(function(el) {
+    container.appendChild(el);
+  });
+
+  return container;
+};
